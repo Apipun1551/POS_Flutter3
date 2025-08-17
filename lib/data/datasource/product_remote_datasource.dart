6aba6aeaac53cab39810ed44138d6240b92e9c34
Afifa Nurhidayah <65105166+Apipun1551@users.noreply.github.com>
2025-08-17 21:10:06 +0700

    Menambahkan sumber data untuk crud

--- lib/data/datasource/product_remote_datasource.dart
+++ lib/data/datasource/product_remote_datasource.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:dartz/dartz.dart';
 import 'package:fic23pos_flutter/core/constants/variables.dart';
 import 'package:fic23pos_flutter/data/datasource/auth_local_datasource.dart';
@@ -14,6 +15,56 @@ class ProductRemoteDatasource {
 
     if (response.statusCode == 200) {
       return right(ProductResponseModel.fromJson(response.body));
+    } else {
+      return left(response.body);
+    }
+  }
+
+  Future<Either<String, String>> addProduct(Map<String, dynamic> product) async {
+    final authData = await AuthLocalDatasource().getAuthData();
+    final response = await http.post(
+      Uri.parse('${Variables.baseUrl}/api/products'),
+      headers: {
+        'Authorization': 'Bearer ${authData?.token}',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(product),
+    );
+
+    if (response.statusCode == 201) {
+      return right('Produk berhasil ditambahkan');
+    } else {
+      return left(response.body);
+    }
+  }
+
+  Future<Either<String, String>> updateProduct(int id, Map<String, dynamic> product) async {
+    final authData = await AuthLocalDatasource().getAuthData();
+    final response = await http.put(
+      Uri.parse('${Variables.baseUrl}/api/products/$id'),
+      headers: {
+        'Authorization': 'Bearer ${authData?.token}',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(product),
+    );
+
+    if (response.statusCode == 200) {
+      return right('Produk berhasil diupdate');
+    } else {
+      return left(response.body);
+    }
+  }
+
+  Future<Either<String, String>> deleteProduct(int id) async {
+    final authData = await AuthLocalDatasource().getAuthData();
+    final response = await http.delete(
+      Uri.parse('${Variables.baseUrl}/api/products/$id'),
+      headers: {'Authorization': 'Bearer ${authData?.token}'},
+    );
+
+    if (response.statusCode == 200) {
+      return right('Produk berhasil dihapus');
     } else {
       return left(response.body);
     }
