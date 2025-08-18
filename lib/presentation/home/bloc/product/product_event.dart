a0067c83a3160262c2d67b1f8258f6774f8f31b2
Afifa Nurhidayah <65105166+Apipun1551@users.noreply.github.com>
2025-08-18 10:34:45 +0700

    Merapikan kode

--- lib/presentation/home/bloc/product/product_event.dart
+++ lib/presentation/home/bloc/product/product_event.dart
@@ -4,11 +4,8 @@ part of 'product_bloc.dart';
 class ProductEvent with _$ProductEvent {
   const factory ProductEvent.started() = _Started;
   const factory ProductEvent.fetchProducts() = _FetchProducts;
-  //products by category
   const factory ProductEvent.fetchProductsByCategory(int categoryId) = _FetchProductsByCategory;
-  //search products
   const factory ProductEvent.searchProducts(String query) = _SearchProducts;
-  //add to cart
   const factory ProductEvent.addProduct(Product product) = _AddProduct;
   const factory ProductEvent.updateProduct(Product product) = _UpdateProduct;
   const factory ProductEvent.deleteProduct(int productId) = _DeleteProduct;
