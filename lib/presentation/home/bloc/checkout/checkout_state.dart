97b041a15c15d921edc41b3f33f8d0a6a38a57e9
Afifa Nurhidayah <65105166+Apipun1551@users.noreply.github.com>
2025-08-24 12:26:54 +0700

    Menambahkan required di success checkout state

--- lib/presentation/home/bloc/checkout/checkout_state.dart
+++ lib/presentation/home/bloc/checkout/checkout_state.dart
@@ -4,7 +4,12 @@ part of 'checkout_bloc.dart';
 class CheckoutState with _$CheckoutState {
   const factory CheckoutState.initial() = Initial;
   const factory CheckoutState.loading() = Loading;
-  const factory CheckoutState.success(List<OrderItem> products, int total, int qty) = Success;
+  // const factory CheckoutState.success(List<OrderItem> products, int total, int qty) = Success;
+  const factory CheckoutState.success({
+    required List<OrderItem> products,
+    required int total,
+    required int qty,
+  }) = Success;
   const factory CheckoutState.failure(String message) = Failure;
   const factory CheckoutState.empty() = Empty;
 }
